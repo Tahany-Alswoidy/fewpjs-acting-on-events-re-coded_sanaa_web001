439eab0c5fa997a47e351759fd1cbdc1cf1df7f5
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-07 13:51:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ function moveDodgerLeft() {
   }
 }
 function moveDodgerRight() {
-  let rightNumbers = dodger.style.right.replace("px", "");
-  let right = parseInt(rightNumbers, 10);
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let ;left = parseInt(rightNumbers, 10);
  
   if (right > 0) {
     dodger.style.right = `${right - 1}px`;
